93a5b69c7c6d4fcaf06dc1412dc644144d258445
bosonking <bosonking@proton.me>
2024-08-12 07:51:29 +0200

    Remove useless console.log

--- src/components/dashboard-page/functional/ChartCard.tsx
+++ src/components/dashboard-page/functional/ChartCard.tsx
@@ -20,42 +20,39 @@ const chartConfig = {
   },
 };
 
-export const ChartCard = ({ data, loading }: Props) => {
-  console.log(data);
-  return (
-    <DataCard
-      icon="ChartLine"
-      title="Hash Rate in last 24 hours"
-      loading={loading}
-    >
-      <ChartContainer config={chartConfig} className="min-h-80 w-full">
-        <LineChart accessibilityLayer data={data}>
-          <CartesianGrid />
-          <XAxis
-            dataKey="dateTime"
-            tickMargin={8}
-            tickFormatter={(value) =>
-              Intl.DateTimeFormat("en", {
-                hour: "numeric",
-                minute: "numeric",
-                hour12: false,
-              }).format(new Date(value))
-            }
-          />
-          <YAxis tickFormatter={(value) => formatHashRate(value)} width={100} />
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent hideLabel />}
-          />
-          <Line
-            dataKey="hashRate"
-            type="natural"
-            stroke="hsl(var(--chart-3))"
-            strokeWidth={2}
-            dot={false}
-          />
-        </LineChart>
-      </ChartContainer>
-    </DataCard>
-  );
-};
+export const ChartCard = ({ data, loading }: Props) => (
+  <DataCard
+    icon="ChartLine"
+    title="Hash Rate in last 24 hours"
+    loading={loading}
+  >
+    <ChartContainer config={chartConfig} className="min-h-80 w-full">
+      <LineChart accessibilityLayer data={data}>
+        <CartesianGrid />
+        <XAxis
+          dataKey="dateTime"
+          tickMargin={8}
+          tickFormatter={(value) =>
+            Intl.DateTimeFormat("en", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: false,
+            }).format(new Date(value))
+          }
+        />
+        <YAxis tickFormatter={(value) => formatHashRate(value)} width={100} />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Line
+          dataKey="hashRate"
+          type="natural"
+          stroke="hsl(var(--chart-3))"
+          strokeWidth={2}
+          dot={false}
+        />
+      </LineChart>
+    </ChartContainer>
+  </DataCard>
+);
