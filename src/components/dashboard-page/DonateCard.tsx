38b50efd9edea751a3a31e95f890466c4267f33c
bosonking <bosonking@proton.me>
2024-08-13 09:38:53 +0200

    Refactor DonateCard

--- src/components/dashboard-page/DonateCard.tsx
+++ src/components/dashboard-page/DonateCard.tsx
@@ -1,5 +1,4 @@
-import onChainPublicPoolAddress from "@/assets/onchain_public_pool.svg";
-import offChainPublicPoolAddress from "@/assets/offchain_public_pool.svg";
+import qrCode from "@/assets/qr-code.svg";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { HandCoins } from "lucide-react";
 import { QrChart } from "./QrChart";
@@ -11,9 +10,9 @@ export const DonateCard = () => {
         <CardTitle className="text-2xl">Donate</CardTitle>
         <HandCoins className="h-8 w-8 text-muted-foreground" />
       </CardHeader>
-      <CardContent className="flex flex-col gap-2">
-        <p className="text-md text-muted-foreground leading-8">
-          This project is open-source and free to use. It is based on{" "}
+      <CardContent className="flex flex-row gap-2 justify-between items-center">
+        <p className="grow text-center text-md text-muted-foreground leading-8">
+          <code>rpool</code> is based on{" "}
           <a
             href="https://github.com/benjamin-wilson/public-pool-ui"
             className="underline"
@@ -23,17 +22,8 @@ export const DonateCard = () => {
           .<br />
           Like the project? Consider a donation.
         </p>
-        <div className="flex flex-row items-center justify-between gap-2">
-          <QrChart
-            src={onChainPublicPoolAddress}
-            alt="bc1q99n3pu025yyu0jlywpmwzalyhm36tg5u37w20d"
-            label="On-chain"
-          />
-          <QrChart
-            src={offChainPublicPoolAddress}
-            alt="public_pool@strike.me"
-            label="Off-chain"
-          />
+        <div className="grow">
+          <QrChart src={qrCode} alt="bosonking@getalby.com" label="Lightning" />
         </div>
       </CardContent>
     </Card>
--- src/components/dashboard-page/QrChart.tsx
+++ src/components/dashboard-page/QrChart.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const QrChart: React.FC<Props> = ({ alt, src, label }) => {
   return (
     <div className="flex flex-col gap-1 items-center justify-center">
-      <img src={src} alt={alt} className="w-24 h-24" />
+      <img src={src} alt={alt} className="w-28 aspect-square" />
       <p className="text-xs text-zinc-600">{label}</p>
     </div>
   );
