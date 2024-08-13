e120791a655ef4fb03e9b948725916bd521626f3
bosonking <bosonking@proton.me>
2024-08-13 15:22:12 +0200

    Fine tunning charts

--- src/components/common/ChartCard.tsx
+++ src/components/common/ChartCard.tsx
@@ -1,35 +1,29 @@
 import {
   ChartConfig,
   ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { HashRateChartData } from "@/domain/chart";
 import { formatHashRate } from "@/lib/formatters";
-import { CartesianGrid, Legend, Line, LineChart, XAxis, YAxis } from "recharts";
-import { DataCard } from "./DataCard";
 import { movingAvg } from "@/lib/moving-avg";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import { DataCard } from "./DataCard";
 
 type ChartDataWithSMA = HashRateChartData & { sma?: number | null };
 
 type Props = {
   data: HashRateChartData[];
   loading?: boolean;
-} & (
-  | {
-      showMovingAverage?: false;
-      movingAveragePeriod?: never;
-    }
-  | {
-      showMovingAverage: true;
-      movingAveragePeriod: number;
-    }
-);
+  movingAveragePeriod?: number;
+};
 
 const chartConfig: ChartConfig = {
   hashRate: {
-    label: "Hash Rate",
+    label: "10 minutes Hash Rate",
     color: "hsl(var(--chart-3))",
   },
 };
@@ -37,36 +31,36 @@ const chartConfig: ChartConfig = {
 export const ChartCard = ({
   data,
   loading,
-  showMovingAverage = true,
   movingAveragePeriod = 12,
 }: Props) => {
   const [chartData, setChartData] = useState<ChartDataWithSMA[]>(
     data.map((v) => ({ ...v, sma: null }))
   );
-  useEffect(() => {
-    if (showMovingAverage) {
-      chartConfig.sma = {
-        label: `SMA${movingAveragePeriod}`,
-        color: "hsl(var(--chart-5))",
-      };
 
-      const sma = movingAvg(
-        data.map((v) => v.hashRate),
-        movingAveragePeriod
-      );
-      setChartData(
-        data.map((v, i) => {
-          if (i % 6 !== 0) return { ...v, sma: null };
-          return { ...v, sma: sma[i] };
-        })
-      );
-    }
-  }, [data, movingAveragePeriod, showMovingAverage]);
+  const hours = useMemo(() => movingAveragePeriod / 6, [movingAveragePeriod]);
+
+  useEffect(() => {
+    chartConfig.sma = {
+      label: `${hours.toLocaleString()} hours Hash Rate`,
+      color: "hsl(var(--chart-5))",
+    };
+
+    const sma = movingAvg(
+      data.map((v) => v.hashRate),
+      movingAveragePeriod
+    );
+    setChartData(
+      data.map((v, i) => {
+        if (i % 6 !== 0) return { ...v, sma: null };
+        return { ...v, sma: sma[i] };
+      })
+    );
+  }, [data, movingAveragePeriod, hours]);
 
   return (
     <DataCard
       icon="ChartLine"
-      title="Hash Rate in last 24 hours"
+      title="Hash Rate in the last 24 hours"
       loading={loading}
       highlight={true}
     >
@@ -75,6 +69,7 @@ export const ChartCard = ({
         className="min-h-80 w-full aspect-square"
       >
         <LineChart accessibilityLayer data={chartData}>
+          <ChartLegend content={<ChartLegendContent />} />
           <CartesianGrid />
           <XAxis
             dataKey="dateTime"
@@ -90,8 +85,29 @@ export const ChartCard = ({
           <YAxis tickFormatter={(value) => formatHashRate(value)} width={100} />
           <ChartTooltip
             cursor={false}
-            content={<ChartTooltipContent hideLabel />}
+            content={
+              <ChartTooltipContent
+                hideLabel
+                indicator="line"
+                formatter={(value, name) => {
+                  const config = chartConfig[name as keyof typeof chartConfig];
+                  return (
+                    <div className="flex min-w-52 items-center text-xs text-muted-foreground gap-2">
+                      <div
+                        className={"h-2 w-2 shrink-0 rounded-[2px]"}
+                        style={{ backgroundColor: config.color }}
+                      ></div>
+                      {config?.label || name}
+                      <div className="ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums text-foreground">
+                        {formatHashRate(+value)}
+                      </div>
+                    </div>
+                  );
+                }}
+              />
+            }
           />
+
           <Line
             dataKey="hashRate"
             type="natural"
@@ -99,19 +115,18 @@ export const ChartCard = ({
             strokeWidth={2}
             dot={false}
           />
-          {showMovingAverage && (
-            <>
-              <Line
-                connectNulls
-                dataKey="sma"
-                type="natural"
-                stroke="hsl(var(--chart-5))"
-                strokeWidth={2}
-                dot={false}
-              />
-              <Legend />
-            </>
-          )}
+          <Line
+            connectNulls
+            dataKey="sma"
+            type="natural"
+            stroke="hsl(var(--chart-5))"
+            strokeWidth={2}
+            dot={{
+              stroke: "hsl(var(--chart-5))",
+              fill: "var(--chart-background)",
+              r: 4,
+            }}
+          />
         </LineChart>
       </ChartContainer>
     </DataCard>
