122c0ee3ebbe0753dd191357c69adde37e46699f
bosonking <bosonking@proton.me>
2024-08-16 09:52:41 +0200

    chore(ci): Add semantic-release

---
+++ release.config.js
@@ -0,0 +1,18 @@
+export default {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/bosonking/rpool-ui.git',
+
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
+};
